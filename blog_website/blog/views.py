724d074ea5c8a58642c655b168de9df7cfc1fd88
Aryan Nath <aryannath2004@gmail.com>
2023-01-05 13:05:27 +0530

    🏠 added home app and 📔 requirements.txt

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return HttpResponse("This is blogHome. We will keep all the blog posts here")
+
+def blogPost(request, slug):
+    return HttpResponse(f"This is blogPost: {slug}")
