ad1d3faf79c994e95c59cfc7e380412a1cb7fe95
Aryan Nath <aryannath2004@gmail.com>
2023-01-05 14:25:45 +0530

    🚀 added markdown templates and base.html

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def blogHome(request):
-    return HttpResponse("This is blogHome. We will keep all the blog posts here")
+    return render(request,'blog/blogHome.html')
 
 def blogPost(request, slug):
-    return HttpResponse(f"This is blogPost: {slug}")
+    return render(request,'blog/blogPost.html',slug)
