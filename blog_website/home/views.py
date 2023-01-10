835a20d3d772ccbef067055cc8c979f87e8a45b6
Aryan Nath <aryannath2004@gmail.com>
2023-01-10 19:13:02 +0530

    👤 added user functionality and blog comment model

--- blog_website/home/views.py
+++ blog_website/home/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, redirect
 from home.models import Contact
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from blog.models import Post
 
-# Create your views here.
+# HTML Pages
 def home(request):
     allPosts = Post.objects.all()
     print(allPosts)
@@ -17,13 +19,10 @@ def contact(request):
         phone = request.POST['phone']
         content = request.POST['content']
         print(name, email, phone, type(phone), content)
-
-        if len(name)==2 or ('.com' not in email or '@' not in email) or (len(phone)<10 or not phone.isnumeric()) or len(content)<3:
-            messages.error(request, 'Please fill the form correctly')
-        else:
-            contact = Contact(name=name, email=email, phone=phone, content=content)
-            contact.save()
-            messages.success(request,'Your message has been successfully sent')
+        
+        contact = Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
+        messages.success(request,'Your message has been successfully sent')
     return render(request,'home/contact.html')
 
 def about(request):
@@ -31,6 +30,58 @@ def about(request):
 
 def search(request):
     query = request.GET['query']
-    allPosts = Post.objects.filter(title__icontains=query)
+    if len(query)>73:
+        allPosts = Post.objects.none()
+    else:
+        allPostsTitle = Post.objects.filter(title__icontains=query)
+        allPostsAuthor = Post.objects.filter(author__icontains=query)
+        allPostsContent = Post.objects.filter(content__icontains=query)
+        allPosts = allPostsTitle.union(allPostsAuthor,allPostsContent)
+    if allPosts.count() == 0:
+        messages.warning(request, 'No search results found')
     params = {'allPosts':allPosts,'query':query} 
     return render(request,'home/search.html',params)
+
+# Authentication APIs
+def handleSignup(request):
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # Check form submission
+        if pass1 != pass2:
+            messages.error(request,'Passwords do not match')
+            return redirect('home') 
+        else:
+            # Create the user
+            myuser = User.objects.create_user(username, email, pass1)
+            myuser.first_name = fname
+            myuser.last_name = lname
+            myuser.save()
+            messages.success(request,'Your account has been successfully created')
+            return redirect('home') 
+    else:
+        return HttpResponse('404 - Not Found')
+def handleLogin(request):
+    if request.method == 'POST':
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginusername, password=loginpassword)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'Successfully logged in')
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid credentials, please try again')
+            return redirect('home')
+    else:
+        return HttpResponse('404 - Not Found')
+def handleLogout(request):
+    logout(request)
+    messages.success(request,'Successfully logged out')
+    return redirect('home')
